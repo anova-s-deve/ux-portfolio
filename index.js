f15426374a8f04750f7dc34586ea7cdd08abc284
Anova Sudeep Deve <41747949+anova-s-deve@users.noreply.github.com>
2020-06-10 12:34:42 +0530

    titles to be added using JS

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function myFunction {
-    document.getElementById("explain")
+var el = document.getElementsByClassName("overlay-inner");
+for (i in el){
+    el[i].addEventListener("mouseover", function(){
+        textVisible();
+    });
+}
+
+function textVisible() {
+    document.getElementsByClassName("top-left-1400").style.opacity = "1";
+    document.getElementsByClassName("overlay-inner").style.backgroundColor = "black";
+    document.getElementsByClassName("overlay-inner").style.opacity = "0.7";
 }
